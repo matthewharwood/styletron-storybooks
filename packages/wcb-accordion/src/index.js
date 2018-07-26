734c3d4e2ba9376b26032ab7941f9dd4412e08d2
Matthew Harwood <Matthhar12@gmail.com>
2018-07-25 21:21:11 -0700

    - Added jest, flow and styletron.

--- packages/wcb-accordion/src/index.js
+++ packages/wcb-accordion/src/index.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 import {styled} from "styletron-react";
 
 const Foo = styled("div", {color: "red"});
-export default class Accordion extends Component {
+export default class Accordion extends Component<*> {
   render() {
 
     return (

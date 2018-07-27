908e20c1157883c05a0581730863b5593a546235
Matthew Harwood <Matthhar12@gmail.com>
2018-07-26 21:44:45 -0700

    fixed storybook with mono repo.

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,16 +1,40 @@
 const path = require("path");
-
+const fs = require('fs');
 // Export a function. Accept the base config as the only param.
-module.exports = (storybookBaseConfig, configType) => {
-  console.log('wtf'.repeat(100), path.resolve('../src/components/dotcom-styles/src'));
+module.exports = (storybookBaseConfig) => {
+  const dirPath = path.join(__dirname, "../src/components");
+  const arrPath = fs.readdirSync(dirPath);
+  const notRight = (main) => {
+    return Boolean(fs.statSync(main));
+  };
 
+  const alias = arrPath.filter((d) => !/^\./.test(d)).reduce((p, v) => {
 
+    try {
+      let PKG = path.join(dirPath, v, 'package.json');
+
+      let pkg;
+      try {
+        pkg = require(PKG);
+      } catch(e) {
+        throw new Error(`please include a package.json inside ${path.join(dirPath, v)}`)
+      }
+      try {
+        p[pkg.name] = path.join(dirPath, v, pkg.main);
+        fs.statSync(p[pkg.name]);
+      } catch(e) {
+        throw new Error(`please update ${PKG} "main: <path-to-index.js>" `)
+      }
+    } catch(e) {
+      console.error(e);
+    }
+
+    return p;
+  }, {});
   storybookBaseConfig.resolve = {
     extensions: ['.js', '.json'],
-    alias: {
-      '@matthewharwood': path.resolve(__dirname, "../src"),
-    },
+    alias,
   };
-  // Return the altered config
+
   return storybookBaseConfig;
 };
